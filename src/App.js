0001795ae17971d8ad49eaecea656a3d22d286ba
Justas Lapiene <Justas63@gmail.com>
2023-03-17 17:41:45 +0100

    added the cards and styled them a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import "./App.scss"
 
 function App() {
   
 
   return (
-    <div>
-      
+    <div className='fatherDiv'>
+      <div className='card first'>
+
+      </div>
+      <div className='card second'>
+        
+      </div>
+      <div className='card third'>
+        
+      </div>
+      <div className='card fourth'>
+        
+      </div>
+      <div className='card fifth'>
+        
+      </div>
+      <div className='card sixth'>
+        
+      </div>
+      <div className='card seventh'>
+        
+      </div>
+      <div className='card eigth'>
+        
+      </div>
+      <div className='card ninth'>
+        
+      </div>
+      <div className='card tenth'>
+        
+      </div>
+      <div className='card eleventh'>
+        
+      </div>
+      <div className='card twelfth'>
+        
+      </div>
     </div>
   );
 }
