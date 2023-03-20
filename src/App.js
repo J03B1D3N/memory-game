0228cab58bf47218b2f283643a5df82aaaa4fe17
Justas Lapiene <Justas63@gmail.com>
2023-03-20 18:15:42 +0100

    bosses now display correctly on the DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "./App.scss"
+import Card from './Card';
 
 function App() {
  const randomNumber = Math.floor(Math.random() * 12 +1)
@@ -10,43 +11,17 @@ console.log(randomNumber)
 
 
   return (
-    <div className='fatherDiv'>
-      <div className='card first'>
+    <div className='papaDiv'>
+      <div className='header'>
 
       </div>
-      <div className='card second'>
-        
-      </div>
-      <div className='card third'>
-        
-      </div>
-      <div className='card fourth'>
-        
-      </div>
-      <div className='card fifth'>
-        
-      </div>
-      <div className='card sixth'>
-        
-      </div>
-      <div className='card seventh'>
-        
-      </div>
-      <div className='card eigth'>
-        
-      </div>
-      <div className='card ninth'>
-        
-      </div>
-      <div className='card tenth'>
-        
-      </div>
-      <div className='card eleventh'>
-        
-      </div>
-      <div className='card twelfth'>
-        
-      </div>
+      <div className='mamaDiv'>
+        {Card()}
+      
+        </div>
+        <div className='footer'>
+
+        </div>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import Ancestral from "./img/AncestralSpirit.png"
 import Astel from "./img/Astel.jpg"
 import Dragonkin from "./img/Dragonkin.png"
-import EldenBeast from "./img/AldenBeast.jpg"
+import EldenBeast from "./img/EldenBeast.jpg"
 import FireGiant from "./img/FireGiant.jpg"
 import Godrick from "./img/Godrick.jpg"
 import MagmaWirm from "./img/MagmaWirm.png"
@@ -13,5 +13,107 @@ import Renala from "./img/Renala.jpg"
 
 import { useState, useEffect } from "react";
 
-const [valSequence, setValSequence] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
-const [isRandomized, setIsRandomized] = useState(false);
+
+function Card(props) {
+
+    const [valSequence, setValSequence] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    const [isRandomized, setIsRandomized] = useState(false);
+    const [charName, setCharName] = useState([
+
+        "Ancestral Spirit",
+        "Astel",
+        "Dragonkin",
+        "Elden Beast",
+        "Fire Giant",
+        "Godrick",
+        "Magma Wirm",
+        "Malenia",
+        "Margit",
+        "Radagon",
+        "Radahn",
+        "Renala"
+
+      ]);
+
+      const [avatar, setAvatar] = useState([
+
+        Ancestral,
+        Astel,
+        Dragonkin,
+        EldenBeast,
+        FireGiant,
+        Godrick,
+        MagmaWirm,
+        Malenia,
+        Margit,
+        Radagon,
+        Radahn,
+        Renala
+
+      ]);
+
+      const [pickedChar, setPickedChar] = useState([
+
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+
+      ]);
+
+
+let recordSet = []
+
+const randomizeVal = () => {
+
+
+    
+    recordSet = []; // our random array that sets the order of the cards
+
+
+    let valSequenceCopy = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // dummy array to be updated with new values
+
+
+    valSequenceCopy.map((e, id) => {
+      let number = Math.floor(Math.random() * (12)); // we rng a number between 0 and 11
+      while (recordSet.includes(number)) {
+        number = Math.floor(Math.random() * (12));
+      } //  if the number is already in the array, we rng another one until we find one out of the 12 that's not yet in the array
+
+      valSequenceCopy[id] = number; // we save the index of valSequenceCopy array we are currently on, in variable "number"
+
+
+      recordSet.push(valSequenceCopy[id]); // we push the value to recordSet array
+
+
+      return number;
+    });
+
+    setValSequence(recordSet); // we update our display array with the new random array.
+    
+  };
+
+  return valSequence.map((num) => {
+    return (
+      
+          <div className="image">
+            <img
+              alt=""
+              src={avatar[num]}
+              id={charName[num]}
+            ></img>
+          </div>
+      
+    );
+  });
+};
+
+export default Card
